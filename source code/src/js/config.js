a185d5d0c5f3b552f970f1a503e41e1ff352f1b5
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2022-04-27 16:19:37 +0530

    Update config.js

--- source code/src/js/config.js
+++ source code/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '80b9d564-0781-433e-b23d-77c86e3c49ec';
+export const KEY = 'add your key here';
 export const MODAL_CLOSE_SEC = 2.5;
